88205e5b3f6887642e5565241599210d00353a45
Victor Kavsjo <kavsjo88@hotmail.se>
2019-10-14 16:13:02 +0200

    demo

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require('express');
-const Firestore = require('@google-cloud/firestore');
+const firebase = require('firebase');
+const db = require('./db');
 
 const app = express();
-const db = new Firestore({
-  projectId: 'api-demo-255407',
-  keyFilename: 'C:/Users/vkavsjo/Downloads/Api-demo-bf38d72a7a8b.json'
-});
+
 customers = db.collection('Customers');
-    
+
+app.get('/', (req, res) => {
+    res.send('Get all customers: /getcustomers-------Get one customer by: /getcustomer/:id-------Customer Ids: 0sTzAeNZT3hGonPrH0Ba, 247I9mi7MfY7lWmcJ5XC, qXzQRXIO6xRFvyXWBbuy')
+});
+
 app.get('/getcustomers', (req, res) => {
-  let allCus = [];
-  customers.get()
+    let allCus = [];
+    customers.get()
     .then((snapshot) => {
         snapshot.forEach((doc) => {
-            allCus.push([
-              doc.id,
-              doc.data().Name
-            ]);
-          })
+            allCus.push(
+                doc.id,
+                doc.data().Name
+            );
+        })
         res.send({
           "Customers": allCus
         })
@@ -45,34 +47,34 @@ app.get('/getcustomer/0sTzAeNZT3hGonPrH0Ba', (req, res) => {
 
 app.get('/getcustomer/247I9mi7MfY7lWmcJ5XC', (req, res) => {
     customers.get()
-      .then((snapshot) => {
-          snapshot.forEach((doc) => {
-              if (doc.id == '247I9mi7MfY7lWmcJ5XC')
-                  res.send([
-                    doc.id,
-                    doc.data()
-                  ]);
-              })
-      })
-      .catch((err) => {
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                if (doc.id == '247I9mi7MfY7lWmcJ5XC')
+                    res.send([
+                        doc.id,
+                        doc.data()
+                ]);
+            })
+        })
+        .catch((err) => {
           console.log('Error getting documents', err);
-      });
-  })
+        });
+})
 
-  app.get('/getcustomer/qXzQRXIO6xRFvyXWBbuy', (req, res) => {
+app.get('/getcustomer/qXzQRXIO6xRFvyXWBbuy', (req, res) => {
     customers.get()
-      .then((snapshot) => {
-          snapshot.forEach((doc) => {
-              if (doc.id == 'qXzQRXIO6xRFvyXWBbuy')
-                  res.send([
-                    doc.id,
-                    doc.data()
-                  ]);
-              })
-      })
-      .catch((err) => {
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                if (doc.id == 'qXzQRXIO6xRFvyXWBbuy')
+                    res.send([
+                        doc.id,
+                        doc.data()
+                    ]);
+            })
+        })
+        .catch((err) => {
           console.log('Error getting documents', err);
-      });
+        });
   })
 
 
@@ -80,10 +82,10 @@ app.get('/getcustomer/247I9mi7MfY7lWmcJ5XC', (req, res) => {
 app.get('/getcus/:id', (req, res) => {
   customers.get().then((snapshot) => {
     snapshot.forEach((doc) => {
-      const _id = req.params.id;
-      const cus =  findOne(_id);
+        const _id = req.params.id;
+        const cus =  findOne(_id);
       
-      if (cus)
+        if (cus)
             res.send([
               doc.id,
               doc.data()
@@ -91,6 +93,23 @@ app.get('/getcus/:id', (req, res) => {
         })
 })
 })   
+app.get('/cus/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+      const cus = await cus.findOne({ _id, owner: req.user._id })
+
+      if (!task) {
+          return res.status(404).send()
+      }
+
+      res.send(cus)
+  } catch (e) {
+      res.status(500).send()
+  }
+})
+
+
 
 app.listen(8080, () =>
   console.log('App listening on port 8080!'),
---
+++ db.js
@@ -0,0 +1,18 @@
+const firebase = require('firebase');
+
+const config = {
+    apiKey: "AIzaSyDh3zZuQRu9N7Cm2IyeSINBlwsfYS6mqdQ",
+    authDomain: "api-demo-255407.firebaseapp.com",
+    databaseURL: "https://api-demo-255407.firebaseio.com",
+    projectId: "api-demo-255407",
+    storageBucket: "api-demo-255407.appspot.com",
+    messagingSenderId: "518471512705",
+    appId: "1:518471512705:web:044e253069a040e5129380",
+    measurementId: "G-JNK14VJLHS"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+module.exports = db;
